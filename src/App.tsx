478958ff9d44d3358d97e552bbe52537a05955c7
Gustavo <gustavo.prog40@gmail.com>
2022-12-01 13:59:31 -0300

    comitando

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,25 @@
 import Buttons from './components/Button'
 import Inputs from './components/Input'
-import { ContainerApp } from './style-app'
+import { ContainerApp, HR, InputsContainer, ImageContainer, LineContainer } from './style-app'
 import astronauta from './assets/astronauta.jpeg'
 
 function App() {
   return (
     <ContainerApp>
-      <div style={{ display: 'flex', width: '100vw' }}>
-        <hr
-          style={{
-            width: '100%',
-            maxWidth: '100%',
-            border: '7px solid darkgreen',
-            margin: '0',
-          }}
-        ></hr>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          width: '100%',
-          position: 'relative',
-        }}
-      >
+      <LineContainer>
+        <HR />
+      </LineContainer>
+      <ImageContainer>
         <img width="290" src={astronauta} alt="astronauta" />
-      </div>
+      </ImageContainer>
 
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: '100%',
-          flexDirection: 'column',
-          marginTop: '40px',
-        }}
-      >
+      <InputsContainer>
         <Inputs type="text" placeholder="Name" />
 
         <Inputs type="email" placeholder="email" />
 
         <Inputs type="password" placeholder="Password" />
-      </div>
- 
+      </InputsContainer>
 
       <Buttons>Botao</Buttons>
     </ContainerApp>
--- src/style-app.tsx
+++ src/style-app.tsx
@@ -1,13 +1,39 @@
 import styled from 'styled-components'
 
 export const ContainerApp = styled.div`
- display:flex;
- align-items: center ;
- flex-direction: column ;
- width:100vw;
- height: 100vh;
- align-items: flex-start;
-          justify-content: top;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  align-items: flex-start;
+  justify-content: top;
 `
 
+export const LineContainer = styled.div`
+  display: flex;
+  width: 100vw;
+`
 
+export const ImageContainer = styled.div`
+  display: flex;
+  width: 100%;
+  position: relative;
+`
+
+export const InputsContainer = styled.div`
+  display: flex;
+  width: 100%;
+
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 40px;
+`
+
+export const HR = styled.hr`
+  width: 100%;
+  max-width: 100%;
+  border: 7px solid darkgreen;
+  margin: 0;
+`
